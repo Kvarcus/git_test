ab14b16ff5fa90a826b0b832a918b95a2bb487e8
unknown <C:\Users\\AppData\Roaming\The Bat!>
2020-11-15 02:28:38 +0300

    maincommit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/main.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Hello</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, tempore minima? Beatae aliquam consequatur inventore quos odio accusantium deserunt? Quas, non explicabo dolorum maxime blanditiis ipsa esse dignissimos neque perferendis.</p>
+</body>
+
+</html>
